e9d2256887b42199c35398f2c0f1acb429112ba0
Chelsy Mooy <chelsymooy@users.noreply.github.com>
2021-04-14 23:36:28 +0700

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,10 +28,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('nakoa.sync.access')->dailyAt('23:30');
-        $schedule->command('nakoa.sync.procure')->dailyAt('23:45');
-        $schedule->command('nakoa.sync.warehouse')->dailyAt('00:30');
-        $schedule->command('nakoa.sync.sale')->dailyAt('01:00');
+        $schedule->command('nakoa:sync:access')->dailyAt('23:45');
+        $schedule->command('nakoa:sync:procure')->dailyAt('00:00');
+        $schedule->command('nakoa:sync:warehouse')->dailyAt('00:30');
+        $schedule->command('nakoa:sync:sale')->dailyAt('00:45');
     }
 
     /**
